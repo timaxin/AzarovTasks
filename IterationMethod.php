4658cf8bb6ea032a5e254eca515fc2c8402afe9e
timaxin <timaxin@gmail.com>
2019-03-04 22:19:33 +0300

    WIP

--- IterationMethod.php
+++ IterationMethod.php
@@ -7,10 +7,43 @@ $multiplierK = 1.1;
 $multiplierN = 0.7;
 
 $initMatrix = [[], [], []];
-for ($i = 1; $i < 4; $i++) {
-    for ($j = 1; $j < 4; $j++) {
-        $initMatrix[$i - 1][] = $multiplierK * $initMatrixFirst[$i - 1][$j - 1] + $multiplierN * $initMatrixSecond[$i - 1][$j - 1];
-        echo $initMatrix[$i - 1][$j - 1] . "<span style='font-size: 16px; line-height: 20px'>x</span>" . "<span style='font-size: 10px'>" . $i . $j . "</span>";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        $initMatrix[$i][] = $multiplierK * $initMatrixFirst[$i][$j] + $multiplierN * $initMatrixSecond[$i][$j];
+        echo $initMatrix[$i][$j] . "<span style='font-size: 16px; line-height: 20px'>x</span>" .
+            "<span style='font-size: 10px; margin-right: 5px'>" . ($j + 1) . "</span>";
     }
-    echo '<br><br>';
+    echo "<span> = " . $valueColumn[$i] . "</span>" . '<br><br>';
+}
+
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        if ($i === $j && checkMethodRule($initMatrix[$i], $initMatrix[$i][$j])) {
+            while (checkMethodRule($initMatrix[$i], $initMatrix[$i][$j])) {
+            }
+        }
+        echo $initMatrix[$i][$j] . "<span style='font-size: 16px; line-height: 20px'>x</span>" .
+            "<span style='font-size: 10px; margin-right: 5px'>" . ($j + 1) . "</span>";
+    }
+    echo "<span> = " . $valueColumn[$i] . "</span>" . '<br><br>';
+}
+
+function absoluteSumOfRow(Array $array):Int {
+    $sum = 0;
+    foreach ($array as $item) {
+        $sum += abs($item);
+    }
+    return $sum;
+}
+
+function checkMethodRule(Array $array, $elem):Bool {
+    return $elem !== max($array) && (absoluteSumOfRow($array) - abs($elem)) > abs($elem);
+}
+
+function arrayAdd(Array $first, Array $second):Array {
+    $result = [];
+    for ($i = 0; $i < count($first); $i++) {
+        $result[] = $first[$i] + $second[$i];
+    }
+    return $result;
 }
