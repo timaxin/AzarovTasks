1d6f6f8dbac8742e6b32ba3e20c2f24d0ed3169e
timaxin <timaxin@gmail.com>
2019-04-16 22:30:53 +0300

    Rewrite code to implement n and k multipliers

--- RungeKutta.php
+++ RungeKutta.php
@@ -6,6 +6,10 @@ $variables = [
     'y' => 0.5,
     'z' => 1
 ];
+$n = 3;
+$k = 3;
+$alpha = 2 + 0.5*$n;
+$beta = 2 + 0.5*$k;
 $x = $beginning;
 $i = 1;
 echo 'Отрезок: [' . $beginning . ', ' . $ending . ']. Шаг h: ' . $h .  '. N = ' . abs($ending - $beginning) / $h . '.<br><br>';
@@ -13,7 +17,7 @@ echo 'Отрезок: [' . $beginning . ', ' . $ending . ']. Шаг h: ' . $h . 
 while ($x <= $ending) {
     $x += $h;
     echo $i . '-ая итерация:<br>x = ' . $x . '<br>';
-    $deltas = getDelta($x, $variables['y'], $variables['z'], $h);
+    $deltas = getDelta($x, $variables['y'], $variables['z'], $h, $alpha, $beta);
     echo 'y<sub>' . ($i - 1) . '</sub> = ' . formatFloat($variables['y']) . ', z<sub>' . ($i - 1) . '</sub> = ' . formatFloat($variables['z']) . '<br>';
     $variables['y'] += $deltas['y'];
     $variables['z'] += $deltas['z'];
@@ -23,11 +27,11 @@ while ($x <= $ending) {
 }
 echo 'Конец промежутка.';
 
-function getDelta ($x, $y, $z, $h) {
-    $k1 = getEquationResult ($x, $y, $z, $h);
-    $k2 = getEquationResult ($x + $h/2, $y + $k1['y']/2, $z + $k1['z']/2, $h);
-    $k3 = getEquationResult ($x + $h/2, $y + $k2['y']/2, $z + $k2['z']/2, $h);
-    $k4 = getEquationResult ($x + $h, $y + $k3['y'], $z + $k3['z'], $h);
+function getDelta ($x, $y, $z, $h, $alpha, $beta) {
+    $k1 = getEquationResult ($x, $y, $z, $h, $alpha, $beta);
+    $k2 = getEquationResult ($x + $h/2, $y + $k1['y']/2, $z + $k1['z']/2, $h, $alpha, $beta);
+    $k3 = getEquationResult ($x + $h/2, $y + $k2['y']/2, $z + $k2['z']/2, $h, $alpha, $beta);
+    $k4 = getEquationResult ($x + $h, $y + $k3['y'], $z + $k3['z'], $h, $alpha, $beta);
 
     return [
         'y' => calculateDelta($k1, $k2, $k3, $k4, 'y'),
@@ -39,13 +43,13 @@ function calculateDelta ($k1, $k2, $k3, $k4, $var) {
     return 1/6 * ($k1[$var] * $k2[$var] * $k3[$var] * $k4[$var]);
 }
 
-function getEquationResult ($x, $y, $z, $h) {
+function getEquationResult ($x, $y, $z, $h, $alpha, $beta) {
     return [
-        'y' => $h * (pow(exp(1), -(pow($y,2) + pow($z, 2))) + 2 * $x),
-        'z' => $h * (2.5 * pow($y, 2) + $z)
+        'y' => $h * (pow(exp(1), -(pow($y,2) + pow($z, 2))) + $alpha * $x),
+        'z' => $h * ($beta * pow($y, 2) + $z)
     ];
 }
 
 function formatFloat ($number) {
-    return number_format($number, 10, '.', ''); // Метод для форматирования вывода (9 знаков после запятой)
+    return number_format($number, 10, '.', '');
 }
